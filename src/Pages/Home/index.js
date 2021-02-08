a007489d9a7edf86bcce4c787dfe891cd9572d78
juniorcintra <junior.developer@outlook.com.br>
2021-02-08 02:31:56 -0300

    rebuild splash screen and onBoarding screen

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,13 +2,14 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {
   ImageBackground,
-  StyleSheet,
   Text,
   View,
   Image,
   TouchableOpacity,
 } from 'react-native';
 import {ButtonDefault} from '../../components';
+
+import styles from './styles';
 
 import ImageBack from '../../assets/images/background.png';
 import imageLogo from '../../assets/images/logonova.png';
@@ -21,6 +22,7 @@ export default function Home({navigation}) {
         <Text style={styles.DescText}>
           Encontre peças automotivas perto de você.
         </Text>
+
         <View style={styles.divBottom}>
           <ButtonDefault
             text="Cadastrar"
@@ -38,58 +40,3 @@ export default function Home({navigation}) {
     </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    maxHeight: 600,
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  DescText: {
-    color: '#fff',
-    fontSize: 22,
-    lineHeight: 27,
-    fontFamily: 'Gilroy-SemiBold',
-    maxWidth: 220,
-    height: 95,
-    textAlign: 'center',
-  },
-  SubDesc: {
-    color: '#fff',
-    fontSize: 15,
-    lineHeight: 18,
-    fontFamily: 'Gilroy-Medium',
-  },
-  logo: {
-    maxWidth: 260,
-    maxHeight: 80,
-  },
-  divBottom: {
-    width: 200,
-    height: 90,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  divLogin: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  login: {
-    color: '#fff',
-    fontSize: 16,
-    lineHeight: 20,
-    fontFamily: 'Gilroy-Bold',
-  },
-});
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,58 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    maxHeight: 600,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  DescText: {
+    color: '#fff',
+    fontSize: 22,
+    lineHeight: 27,
+    fontFamily: 'Gilroy-SemiBold',
+    width: 190,
+    height: 95,
+    textAlign: 'center',
+  },
+  SubDesc: {
+    color: '#fff',
+    fontSize: 15,
+    lineHeight: 18,
+    fontFamily: 'Barlow-Medium',
+  },
+  logo: {
+    maxWidth: 260,
+    maxHeight: 80,
+  },
+  divBottom: {
+    width: 200,
+    height: 90,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  divLogin: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  login: {
+    color: '#fff',
+    fontSize: 16,
+    lineHeight: 20,
+    fontFamily: 'Gilroy-Bold',
+  },
+});
+
+export default styles;
